92a47c58ef6206032526b6b5faa84eb7a8b13691
Rui <rui95fer@gmail.com>
2024-10-25 15:07:38 +0100

    feat(cart): implements Cart Page, Remove from Cart, and Clear Cart functionality

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,36 +1,41 @@
-import {createBrowserRouter} from "react-router-dom";
-import App from "../App.jsx";
-import Home from "../pages/home/Home.jsx";
-import {Login} from "../components/Login.jsx";
-import {Register} from "../components/Register.jsx";
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App.jsx'
+import Home from '../pages/home/Home.jsx'
+import { Login } from '../components/Login.jsx'
+import { Register } from '../components/Register.jsx'
+import { CartPage } from '../pages/books/CartPage.jsx'
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <App/>,
+        path: '/',
+        element: <App />,
         children: [
             {
-                path: "/",
-                element: <Home/>
+                path: '/',
+                element: <Home />,
             },
             {
-                path: "/orders",
-                element: <div>Orders</div>
+                path: '/orders',
+                element: <div>Orders</div>,
             },
             {
-                path: "/about",
-                element: <div>About</div>
+                path: '/about',
+                element: <div>About</div>,
             },
             {
-                path: "/login",
-                element: <Login/>
+                path: '/login',
+                element: <Login />,
             },
             {
-                path: "/register",
-                element: <Register/>
-            }
-        ]
+                path: '/register',
+                element: <Register />,
+            },
+            {
+                path: '/cart',
+                element: <CartPage />,
+            },
+        ],
     },
-]);
+])
 
-export default router;
+export default router
