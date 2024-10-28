33b0cfc0e4fc2b1d8ad92fbf270398ee70c41f5c
Rui <rui95fer@gmail.com>
2024-10-28 14:35:48 +0000

    feat: add checkout page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,22 @@
-import {Link} from "react-router-dom";
-import {useState} from "react";
-import {FaGoogle} from "react-icons/fa";
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { FaGoogle } from 'react-icons/fa'
 
-import {useForm} from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
 export const Login = () => {
     const [message, setMessage] = useState()
 
-    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = (data) => console.log(data)
 
-    const handleGoogleSignIn = () => {
-
-    }
+    const handleGoogleSignIn = () => {}
 
     return (
         <div className="h-[calc(100vh-120px)] flex justify-center items-center">
@@ -22,45 +25,76 @@ export const Login = () => {
 
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-                        <input {...register("email", {required: true})} type="email" name="email" id="email"
-                               placeholder="Email Address"
-                               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"/>
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="email"
+                        >
+                            Email
+                        </label>
+                        <input
+                            {...register('email', { required: true })}
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Email Address"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                        />
                     </div>
                     <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2"
-                               htmlFor="password">Password</label>
-                        <input {...register("password", {required: true})} type="password" name="password" id="password"
-                               placeholder="Password"
-                               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"/>
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
+                        <input
+                            {...register('password', { required: true })}
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Password"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                        />
                     </div>
 
-                    {
-                        message && <p className="text-red-500 text-xs italic mb-3">{message}</p>
-                    }
+                    {message && (
+                        <p className="text-red-500 text-xs italic mb-3">
+                            {message}
+                        </p>
+                    )}
 
                     <div className="mb-4">
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">Login
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">
+                            Login
                         </button>
                     </div>
                 </form>
 
-                <p className="align-baseline font-medium mt-4 text-sm">Don't have an account?
-                    <Link to="/register" className="text-blue-500 hover:text-blue-700"> Sign up</Link>.
+                <p className="align-baseline font-medium mt-4 text-sm">
+                    Don't have an account?
+                    <Link
+                        to="/register"
+                        className="text-blue-500 hover:text-blue-700"
+                    >
+                        {' '}
+                        Sign up
+                    </Link>
+                    .
                 </p>
 
                 <div className="mt-4">
                     <button
                         onClick={handleGoogleSignIn}
-                        className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
-                        <FaGoogle className="mr-2"/>
+                        className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+                    >
+                        <FaGoogle className="mr-2" />
                         Sign in with Google
                     </button>
                 </div>
 
-                <p className="mt-5 text-center text-gray-500 text-xs">© 2024 Book Store</p>
-
+                <p className="mt-5 text-center text-gray-500 text-xs">
+                    © 2024 Book Store
+                </p>
             </div>
         </div>
     )
